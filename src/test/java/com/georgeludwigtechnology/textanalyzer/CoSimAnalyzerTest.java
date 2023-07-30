d632efbfec4a64c3b9cefc082119607b31013664
georgeludwig <georgeludwig@gmail.com>
2023-07-30 09:28:42 -0700

    made test string constants, adjusted doc constructors, print results after first test analysis

--- src/test/java/com/georgeludwigtechnology/textanalyzer/CoSimAnalyzerTest.java
+++ src/test/java/com/georgeludwigtechnology/textanalyzer/CoSimAnalyzerTest.java
@@ -8,37 +8,40 @@ import java.util.List;
 
 
 public class CoSimAnalyzerTest {
-	
+
+	static final String DOC1 = "LSI tutorials and fast tracks.";
+	static final String DOC2 = "Books on #semantic analysis.";
+	static final String DOC3 = "Learning latent #semantic indexing.";
+	static final String DOC4 = "Advances in structures and advances in indexing.";
+	static final String DOC5 = "Analysis of latent structures.";
+	static final String QUERY = "latent #semantic indexing";
+
 	@BeforeEach
 	public void setUp() throws Exception {}
-	
+
+
 	@Test
 	public void testApp() throws Exception {
+		// NOTE: test values were painstakingly, manually calculated
+		// set global Document optional params
+		Document.setCleanPunctuation(true);
+		Document.setUseStemming(true);
+		Document.setRemoveStopWords(true);
 		// create the analyzer
 		CoSimAnalyzer analyzer=new CoSimAnalyzer();
-		// set optional doc params
-//		Document.setCleanPunctuation(true);
-//		Document.setUseStemming(true);
-//		Document.setRemoveStopWords(true);
-		// create the docs
-		Document doc1=new Document(true,true,true);
-		doc1.setText("LSI tutorials and fast tracks.");
+		// create the docs...params are cleanPunctuation, useStemming, removeStopWords
+		Document doc1=new Document(DOC1);
 		analyzer.addDocument(doc1);
-		Document doc2=new Document(true,true,true);
-		doc2.setText("Books on #semantic analysis.");
+		Document doc2=new Document(DOC2);
 		analyzer.addDocument(doc2);
-		Document doc3=new Document(true,true,true);
-		doc3.setText("Learning latent #semantic indexing.");
+		Document doc3=new Document(DOC3);
 		analyzer.addDocument(doc3);
-		Document doc4=new Document(true,true,true);
-		doc4.setText("Advances in structures and advances in indexing.");
+		Document doc4=new Document(DOC4);
 		analyzer.addDocument(doc4);
-		Document doc5=new Document(true,true,true);
-		doc5.setText("Analysis of latent structures.");
+		Document doc5=new Document(DOC5);
 		analyzer.addDocument(doc5);
 		// create the query
-		Document query=new Document(true,true,true);
-		query.setText("latent #semantic indexing");
+		Document query=new Document(QUERY);
 		analyzer.setQuery(query);
 		// analyze
 		analyzer.analyze();
@@ -48,13 +51,17 @@ public class CoSimAnalyzerTest {
 		List<Word>sortedWords=analyzer.getSortedWords();
 		Assertions.assertTrue(sortedWords.get(0).getWord().contains("analysi"));
 		Assertions.assertTrue(sortedWords.get(0).getWeight()==1.0208020694198054);
-		Assertions.assertTrue(doc1.getOriginalText().equals("LSI tutorials and fast tracks."));
-		Assertions.assertTrue(doc2.getOriginalText().equals("Books on #semantic analysis."));
-		Assertions.assertTrue(doc3.getOriginalText().equals("Learning latent #semantic indexing."));
-		Assertions.assertTrue(doc4.getOriginalText().equals("Advances in structures and advances in indexing."));
-		Assertions.assertTrue(doc5.getOriginalText().equals("Analysis of latent structures."));
-		Assertions.assertTrue(query.getOriginalText().equals("latent #semantic indexing"));
-		
+		Assertions.assertTrue(doc1.getOriginalText().equals(DOC1));
+		Assertions.assertTrue(doc2.getOriginalText().equals(DOC2));
+		Assertions.assertTrue(doc3.getOriginalText().equals(DOC3));
+		Assertions.assertTrue(doc4.getOriginalText().equals(DOC4));
+		Assertions.assertTrue(doc5.getOriginalText().equals(DOC5));
+		Assertions.assertTrue(query.getOriginalText().equals(QUERY));
+
+		analyzer.getDocuments().stream().forEach(d -> System.out.println(d));
+		System.out.println(query);
+		analyzer.getSortedWords().stream().forEach(w -> System.out.println(w));
+
 		analyzer.reset();
 		
 		// re-analyze
@@ -65,12 +72,12 @@ public class CoSimAnalyzerTest {
 		sortedWords=analyzer.getSortedWords();
 		Assertions.assertTrue(sortedWords.get(0).getWord().contains("analysi"));
 		Assertions.assertTrue(sortedWords.get(0).getWeight()==1.0208020694198054);
-		Assertions.assertTrue(doc1.getOriginalText().equals("LSI tutorials and fast tracks."));
-		Assertions.assertTrue(doc2.getOriginalText().equals("Books on #semantic analysis."));
-		Assertions.assertTrue(doc3.getOriginalText().equals("Learning latent #semantic indexing."));
-		Assertions.assertTrue(doc4.getOriginalText().equals("Advances in structures and advances in indexing."));
-		Assertions.assertTrue(doc5.getOriginalText().equals("Analysis of latent structures."));
-		Assertions.assertTrue(query.getOriginalText().equals("latent #semantic indexing"));
+		Assertions.assertTrue(doc1.getOriginalText().equals(DOC1));
+		Assertions.assertTrue(doc2.getOriginalText().equals(DOC2));
+		Assertions.assertTrue(doc3.getOriginalText().equals(DOC3));
+		Assertions.assertTrue(doc4.getOriginalText().equals(DOC4));
+		Assertions.assertTrue(doc5.getOriginalText().equals(DOC5));
+		Assertions.assertTrue(query.getOriginalText().equals(QUERY));
 	}
 	
 	@AfterEach
